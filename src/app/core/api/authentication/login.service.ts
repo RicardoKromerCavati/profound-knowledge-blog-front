07f105b82070ab0c9b1fa82c020be6f2ed5e29fd
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2026-06-11 22:56:00 -0300

    Handle error response on service.

--- src/app/core/api/authentication/login.service.ts
+++ src/app/core/api/authentication/login.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { UserLoginResponse } from '../authentication/user.login.response';
 import { UserLoginRequest } from '../authentication/user.login.request';
 import { environment } from '../../../../environments/environment';
 import { AuthService } from './auth.service';
+import { ApiErrorResponse } from '../shared/api.error.response';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,15 @@ export class LoginService {
         tap((response) => {
           localStorage.setItem('token', response.token);
           this.authService.currentUserSignal.set(response);
+        }),
+        catchError((httpError: HttpErrorResponse) => {
+          let errorMessage = "Erro inesperado";
+
+          if (httpError.error && (httpError.error as ApiErrorResponse).detail) {
+            errorMessage = (httpError.error as ApiErrorResponse).detail;
+          }
+
+          return throwError(() => errorMessage);
         })
       );
   }
